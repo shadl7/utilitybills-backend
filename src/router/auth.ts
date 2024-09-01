fc1537130745646f16d8aba3ccd3430b50b96a94
Shadl7 <tupjnin@gmail.com>
2024-09-01 23:25:19 +0700

    add prisma

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,7 +1,8 @@
 // Main router of dashboard
 import express from "express";
+import {loginService} from "../service/auth/login";
 export const authRouter = express.Router();
 
-authRouter.get("/auth", (req, res, next) => {
+authRouter.get("/auth", (req : express.Request, res: express.Response, next: express.NextFunction) => {
 
 });
---
+++ src/service/auth/login.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+class LoginService {
+  login(req : express.Request, res: express.Response) {
+
+  }
+}
+
+export const loginService = new LoginService();
